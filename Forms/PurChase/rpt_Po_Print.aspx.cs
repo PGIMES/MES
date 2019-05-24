7e6271401718ff23ba30b30a85cb76d25f4214d0
heguiqin <guiqin_he@163.com>
2019-05-24 10:21:46 +0800

    PO打印

--- Forms/PurChase/rpt_Po_Print.aspx.cs
+++ Forms/PurChase/rpt_Po_Print.aspx.cs
@@ -11,12 +11,45 @@ public partial class Forms_PurChase_rpt_Po_Print : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string pono = "";
+        if (Request.QueryString["pono"] != null) { pono = Request.QueryString["pono"]; }
+        LoginUser LogUserModel = InitUser.GetLoginUserInfo("", Request.ServerVariables["LOGON_USER"]);
+
+        //----------------------------------------------------打印记录----------------------------------------------------------------------
+        //定义总SQL LIST
+        List<Pgi.Auto.Common> ls_sum = new List<Pgi.Auto.Common>();
+
+        Pgi.Auto.Common ls_update = new Pgi.Auto.Common();
+        string sql_update = @"update PUR_PO_Main_Form set IsPrint=1 where PoNo='{0}' and isnull(IsPrint,'')=''";
+        sql_update = string.Format(sql_update, pono);
+        ls_update.Sql = sql_update;
+        ls_sum.Add(ls_update);
+
+        Pgi.Auto.Common ls_insert = new Pgi.Auto.Common();
+        string sql_insert = @"insert into PUR_PO_Print_His(PoNo, PrintById, PrintByName)
+                            select '{0}','{1}','{2}'";
+        sql_insert = string.Format(sql_insert, pono, LogUserModel.UserId, LogUserModel.UserName);
+        ls_insert.Sql = sql_insert;
+        ls_sum.Add(ls_insert);
+
+        int ln = Pgi.Auto.Control.UpdateListValues(ls_sum);
+        //string msg = "";
+        //if (ln > 0)
+        //{
+        //    msg = "确认成功！";
+        //}
+        //else
+        //{
+        //    msg = "确认失败！";
+        //}
+        //string lsstr = "layer.alert('" + msg + "',function(index) {parent.layer.close(index);parent.location.reload();})";
+        //ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", lsstr, true);
+        //--------------------------------------------------------------------------------------------------------------------------
+
         FastReport.Report report = this.WebReport1.Report;
         report.Load(Server.MapPath("Po_Print.frx"));//加载模板
         string lssql = "exec rpt_Po_Print '{0}', '{1}'";
 
-        string pono = "";
-        if (Request.QueryString["pono"] != null) { pono = Request.QueryString["pono"]; }
 
         string lssql_m = string.Format(lssql, pono,"main");
         DataTable ldt = DbHelperSQL.Query(lssql_m).Tables[0];
