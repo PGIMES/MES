e4c98bc0dad5475399e7086ab8bdb15f99677427
heguiqin <guiqin_he@163.com>
2019-05-28 08:30:18 +0800

    ok

--- Forms/PurChase/PUR_PRCategoryDtlMaintain.aspx.cs
+++ Forms/PurChase/PUR_PRCategoryDtlMaintain.aspx.cs
@@ -7,16 +7,47 @@ using System.Web.UI.WebControls;
 using Maticsoft.DBUtility;
 public partial class Forms_PurChase_PUR_PRCategoryDtlMaintain : System.Web.UI.Page
 {
+    public string isIT="false";
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!Page.IsPostBack)
+        {
+            LoginUser LogUserModel = InitUser.GetLoginUserInfo("", Request.ServerVariables["LOGON_USER"]);
+            Session["LogUser"] = LogUserModel;
+            Session["UserAD"] = LogUserModel.ADAccount;
+            Session["UserId"] = LogUserModel.UserId;
+            if (!LogUserModel.DepartName.Contains("IT"))
+            {
+                isIT = "false";
+                GridView1.Columns[8].Visible = false;
+                setshow(false);
+            }
+            else
+            {
+                isIT = "true";
+                GridView1.Columns[8].Visible = true;
+                setshow(true);
+            }
 
+
+        }
+
+    }
+
+    private void setshow(bool bln)
+    {
+        for(int i = 0; i < GridView1.Rows.Count; i++)
+        {
+            (GridView1.Rows[i].FindControl("txtdeptcode") as TextBox).Visible=bln;
+            (GridView1.Rows[i].FindControl("lbldeptcode") as Label).Visible = !bln;
+            (GridView1.Rows[i].FindControl("txtdeptname") as TextBox).Visible = bln;
+            (GridView1.Rows[i].FindControl("lbldeptname") as Label).Visible =! bln;
+        }
     }
 
     protected void btnSave_Click(object sender, EventArgs e)
     {
-        
-
-        
+            
 
     }
 
