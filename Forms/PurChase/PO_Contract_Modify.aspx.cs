df1037234b1c3c18a26cab8fd72700251e5eb3f3
heguiqin <guiqin_he@163.com>
2019-11-20 09:47:02 +0800

    Update PO_Contract_Modify.aspx.cs

--- Forms/PurChase/PO_Contract_Modify.aspx.cs
+++ Forms/PurChase/PO_Contract_Modify.aspx.cs
@@ -80,14 +80,14 @@ public partial class Forms_PurChase_PO_Contract_Modify : System.Web.UI.Page
         Pgi.Auto.Common ls_update = new Pgi.Auto.Common();
         string sql_update = @"update PUR_PO_ContractPay_Plan_His set b_flag=0,UpdateId='{2}',UpdateName='{3}',UpdateTime=getdate() 
                             where domain='{0}' and SysContractNo='{1}' and ContractLine is null and b_flag=1";
-        sql_update = string.Format(sql_update, domain, nbr, LogUserModel.UserId, LogUserModel.UserName);
+        sql_update = string.Format(sql_update, txt_domain.Text, SysContractNo.Text, LogUserModel.UserId, LogUserModel.UserName);
         ls_update.Sql = sql_update;
         ls_sum.Add(ls_update);
 
         Pgi.Auto.Common ls_insert = new Pgi.Auto.Common();
         string sql_insert = @"insert into PUR_PO_ContractPay_Plan_His(domain,SysContractNo, SignDate,CreateId,CreateName,CreateTime,b_flag)
                        select '{0}','{1}','{2}','{3}','{4}',getdate(),'1'";
-        sql_insert = string.Format(sql_insert, domain, nbr, SignDate.Value, LogUserModel.UserId, LogUserModel.UserName);
+        sql_insert = string.Format(sql_insert, txt_domain.Text, SysContractNo.Text, SignDate.Value, LogUserModel.UserId, LogUserModel.UserName);
         ls_insert.Sql = sql_insert;
         ls_sum.Add(ls_insert);
 
