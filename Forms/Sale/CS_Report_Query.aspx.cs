218ef00728f7e1f075487b4d670912924b1ebd8e
heguiqin <guiqin_he@163.com>
2019-11-22 09:01:33 +0800

    客户日程单浏览

--- Forms/Sale/CS_Report_Query.aspx.cs
+++ Forms/Sale/CS_Report_Query.aspx.cs
@@ -1,14 +1,58 @@
-﻿using System;
+﻿using Maticsoft.DBUtility;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 public partial class Forms_Sale_CS_Report_Query : System.Web.UI.Page
 {
+    //public string UserId = "";
+    //public string UserName = "";
+    //public string DeptName = "";
+    //LoginUser LogUserModel = null;
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        //LogUserModel = InitUser.GetLoginUserInfo("", Request.ServerVariables["LOGON_USER"]);
+        //UserId = LogUserModel.UserId;
+        //UserName = LogUserModel.UserName;
+        //DeptName = LogUserModel.DepartName;
 
+        if (!IsPostBack)
+        {
+            
+        }
+        QueryASPxGridView();
+    }
+
+    protected void Bt_select_Click(object sender, EventArgs e)
+    {
+        QueryASPxGridView();
+        ScriptManager.RegisterStartupScript(this, e.GetType(), "merge", "setHeight();", true);
+    }
+
+    public void QueryASPxGridView()
+    {
+        string sql = @"exec Report_CS '{0}','{1}'";
+        sql = string.Format(sql, ddl_comp.SelectedValue, txt_wlh.Text);
+
+        DataTable dt = DbHelperSQL.Query(sql).Tables[0];
+        GV_PART.DataSource = dt;
+        GV_PART.DataBind();
+    }
+    protected void Bt_Export_Click(object sender, EventArgs e)
+    {
+        QueryASPxGridView();
+        GV_PART.ExportXlsxToResponse("客户日程单浏览_" + System.DateTime.Now.ToString("yyyyMMdd"), new DevExpress.XtraPrinting.XlsxExportOptionsEx { ExportType = DevExpress.Export.ExportType.WYSIWYG });//导出到Excel
+    }
+
+    protected void GV_PART_PageIndexChanged(object sender, EventArgs e)
+    {
+        QueryASPxGridView();
+        ScriptManager.RegisterStartupScript(this, e.GetType(), "", "setHeight() ;", true);
     }
 }
